e857b8a1abd4237f10d789e6f8f39a65b3e3081a
Pedro Lopez <39785338+plopez9@users.noreply.github.com>
2019-03-29 09:11:05 -0500

    3/29/19

--- sporting_webapp/sporting_webapp/settings.py
+++ sporting_webapp/sporting_webapp/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+    }
+}
 
 # Application definition
 
