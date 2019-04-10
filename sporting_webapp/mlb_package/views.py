78d9c6faf547cec3bc39615920292c5767124c29
Pedro Lopez <39785338+plopez9@users.noreply.github.com>
2019-04-10 16:22:28 -0500

    4/10/19
    Rest of Front End Pages

--- sporting_webapp/mlb_package/views.py
+++ sporting_webapp/mlb_package/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to the Show!")
+    return render(request, "mlb_page.html")
--- sporting_webapp/nfl_package/views.py
+++ sporting_webapp/nfl_package/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "construction_page.html")
+    return render(request, "nfl_page.html")
